f68678abfe8eb929b18298e0042e762b6e8e6802
Zen 0ne <gor.sharoyan95@gmail.com>
2022-03-26 01:42:13 +0400

    :green_heart:CI(components/Footer&Header):created Header and Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <ol className="list">
+        <li>Copyright : Gor Sharoyan 2022</li>
+        <li>Email :gor.sharoyan95@gmail.com</li>
+      </ol>
+    </footer>
+  );
+}
